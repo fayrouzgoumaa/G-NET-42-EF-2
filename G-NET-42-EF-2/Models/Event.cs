0925ae4f2c7baf5f6ae2017a00807b2b3b95e880
fayro <fayro@FAYROUZGOUMAA>
2026-05-13 00:43:43 +0300

    Event class

--- G-NET-42-EF-2/Models/Event.cs
+++ G-NET-42-EF-2/Models/Event.cs
@@ -8,5 +8,38 @@ namespace G_NET_42_EF_2.Models
 {
     internal class Event
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int MaxAttendees { get; set; }
+
+        // Organizer Relationship
+        public int OrganizerId { get; set; }
+
+        public Organizer Organizer { get; set; }
+
+        // Self Relationship
+        public int? ParentEventId { get; set; }
+
+        public Event ParentEvent { get; set; }
+
+        public ICollection<Event> Sessions { get; set; }
+            = new List<Event>();
+
+        // Many-to-Many
+        public ICollection<Registration> Registrations { get; set; }
+            = new List<Registration>();
+
+        // Audit Fields
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }

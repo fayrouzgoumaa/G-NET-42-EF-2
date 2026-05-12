791345d554b0ef37132c5d0ec71090681b4808a7
fayro <fayro@FAYROUZGOUMAA>
2026-05-13 00:44:54 +0300

    Attende class

--- G-NET-42-EF-2/Models/Attendee.cs
+++ G-NET-42-EF-2/Models/Attendee.cs
@@ -8,5 +8,15 @@ namespace G_NET_42_EF_2.Models
 {
     internal class Attendee
     {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public Address Address { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+            = new List<Registration>();
     }
 }

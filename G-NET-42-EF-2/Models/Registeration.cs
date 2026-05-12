865ec15bd50a0aa202af13c2fd83be830c1ac7e4
fayro <fayro@FAYROUZGOUMAA>
2026-05-13 00:45:34 +0300

    Registration class

--- G-NET-42-EF-2/Models/Registeration.cs
+++ G-NET-42-EF-2/Models/Registeration.cs
@@ -8,5 +8,25 @@ namespace G_NET_42_EF_2.Models
 {
     internal class Registeration
     {
+        public int Id { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public string? Note { get; set; }
+
+        public string Tier { get; set; }
+
+        // FK
+        public int AttendeeId { get; set; }
+
+        public Attendee Attendee { get; set; }
+
+        // FK
+        public int EventId { get; set; }
+
+        public Event Event { get; set; }
+
+        // One-to-One
+        public Badge Badge { get; set; }
     }
 }
